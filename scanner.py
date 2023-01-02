c590f9642211d7a27492abcdc873948f8042d654
daredever <daredever@gmail.com>
2023-01-02 12:24:49 +0300

    fix scanner range for int

--- scanner.py
+++ scanner.py
@@ -1,17 +1,17 @@
 def get_int(start: int, end: int, prompt: str) -> int:
     """
-    Gets integer value from user input in range [start, end).
+    Gets integer value from user input in range [start, end].
     """
     assert isinstance(start, int)
     assert isinstance(end, int)
     assert isinstance(prompt, str)
-    
+
     while True:
         try:
             value = int(input(prompt))
-            assert start <= value < end
+            assert start <= value <= end
             return value
         except ValueError:
             print('Value should be an integer')
         except AssertionError:
-            print(f'Value should be an integer in range [{start}, {end})')
+            print(f'Value should be an integer in range [{start}, {end}]')
--- x_files_links_generator.py
+++ x_files_links_generator.py
@@ -4,12 +4,15 @@ import scanner
 def main():
     # See more about all seasons https://en.wikipedia.org/wiki/The_X-Files
     series = [24, 25, 24, 24, 20, 22, 22, 21, 20, 6, 10]
-    season = scanner.get_int(1, len(series) + 1, 'What\'s season? ')
+    season = scanner.get_int(1, len(series), 'What\'s season? ')
     for episode in episodes(series[season - 1]):
         print(f"episode {episode}:", get_hyperlink(season, episode))
 
 
 def episodes(n):
+    """
+    Generates a sequence of episodes in range [1, n].
+    """
     for i in range(1, n + 1):
         yield i
 
